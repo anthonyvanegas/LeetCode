8d61e6ee24b618170834f06a1c276a15725876df
anthonyvanegas <antvanegas@icloud.com>
2024-07-28 17:23:17 -0400

    Completed Palindrome Number

--- Problems/9. Palindrome Number/Solution.py
+++ Problems/9. Palindrome Number/Solution.py
@@ -1,3 +1,17 @@
+# Submission Detail - https://leetcode.com/submissions/detail/1336664299/
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        numStr = str(x)
+        if numStr[0] != "-":
+            upperIndex = len(numStr) - 1
+            midIndex = (upperIndex + 2 - 1) // 2
+            leftPart = numStr[0:midIndex]
+            rightPart = numStr[midIndex+1:upperIndex+1]
+            if len(numStr) % 2 != 0:
+                rightPart = numStr[midIndex+1:upperIndex+1]
+            else:
+                rightPart = numStr[midIndex:upperIndex+1]
+            if leftPart == rightPart[::-1]:
+                return True
         return False
--- Problems/9. Palindrome Number/Testcase.py
+++ Problems/9. Palindrome Number/Testcase.py
@@ -8,9 +8,30 @@ class TestSuite(unittest.TestCase):
 
     def test_example1(self):
         input = {
-            'x': 10
+            'x': -10
         }
         output = False
+        self.assertEqual(self.solution.isPalindrome(input['x']), output)
+
+    def test_example2(self):
+        input = {
+            'x': 101
+        }
+        output = True
+        self.assertEqual(self.solution.isPalindrome(input['x']), output)
+
+    def test_example3(self):
+        input = {
+            'x': 11
+        }
+        output = True
+        self.assertEqual(self.solution.isPalindrome(input['x']), output)
+
+    def test_example4(self):
+        input = {
+            'x': 121
+        }
+        output = True
         self.assertEqual(self.solution.isPalindrome(input['x']), output)
 
 if __name__ == '__main__':
