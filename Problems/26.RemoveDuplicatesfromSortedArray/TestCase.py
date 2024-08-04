baf39d0ae0fcc673fd4a6faedf56c20147577147
anthonyvanegas <antvanegas@icloud.com>
2024-08-04 17:58:34 -0400

    Initial setup and folder renames

---
+++ Problems/26.RemoveDuplicatesfromSortedArray/TestCase.py
@@ -0,0 +1,23 @@
+import unittest
+from Solution import Solution
+
+
+class TestSuite(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.solution = Solution()
+
+    def test_example1(self):
+        input = {
+            "nums": [1, 1, 2],
+            }
+        output = 2
+        self.assertEqual(
+                self.solution.removeDuplicates(input["nums"]),
+                output,
+                f"{input["nums"]} - Test Case Failed")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
