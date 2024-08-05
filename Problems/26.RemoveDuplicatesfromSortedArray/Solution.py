3aa16cabc79f4074c7e2e2864b08c686db6f1227
anthonyvanegas <antvanegas@icloud.com>
2024-08-05 14:48:30 -0400

    Switching over to two pointer solution

--- Problems/26.RemoveDuplicatesfromSortedArray/Solution.py
+++ Problems/26.RemoveDuplicatesfromSortedArray/Solution.py
@@ -3,11 +3,18 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
+        #Check if greater
+        #Then swap position after
         ubound = len(nums) - 1
-        print(ubound)
-        for i in range(ubound, 1, -1):
-            if nums[i] == nums[i-1]:
-                nums[i] = "_"
-        print(nums)
-        return 1
-
+        i = 0
+        while i <= ubound - 1:
+            pointer_1 = nums[i]
+            j = i + 1
+            while j <= ubound:
+                pointer_2 = nums[j]
+                if pointer_2 > pointer_1:
+                    nums[i] = pointer_2
+                    nums[j] = pointer_1
+                    break
+            i += 1
+        return i + 1
