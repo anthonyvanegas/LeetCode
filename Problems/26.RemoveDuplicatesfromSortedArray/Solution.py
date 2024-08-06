5923256a97c226fa8cad09ec1f95c7cf1f7346d2
anthonyvanegas <antvanegas@icloud.com>
2024-08-05 20:35:43 -0400

    Completed problem #26

--- Problems/26.RemoveDuplicatesfromSortedArray/Solution.py
+++ Problems/26.RemoveDuplicatesfromSortedArray/Solution.py
@@ -1,20 +1,14 @@
+# Submission Detail - https://leetcode.com/submissions/detail/1345967507/
+# Credit - https://www.youtube.com/watch?v=DEJAZBq0FDA
+
 from typing import List
 
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        #Check if greater
-        #Then swap position after
-        ubound = len(nums) - 1
-        i = 0
-        while i <= ubound - 1:
-            pointer_1 = nums[i]
-            j = i + 1
-            while j <= ubound:
-                pointer_2 = nums[j]
-                if pointer_2 > pointer_1:
-                    nums[i] = pointer_2
-                    nums[j] = pointer_1
-                    break
-            i += 1
-        return i + 1
+        L = 1
+        for R in range(1, len(nums)):
+            if nums[R] != nums[R - 1]:
+                nums[L] = nums[R]
+                L += 1
+        return L
--- Problems/26.RemoveDuplicatesfromSortedArray/TestCase.py
+++ Problems/26.RemoveDuplicatesfromSortedArray/TestCase.py
@@ -17,6 +17,16 @@ class TestSuite(unittest.TestCase):
                 output,
                 f"{input["nums"]} - Test Case Failed")
 
+    def test_example2(self):
+        input = {
+            "nums": [0, 0, 1, 1, 1, 2, 2, 3, 3, 4],
+            }
+        output = 5
+        self.assertEqual(
+                self.solution.removeDuplicates(input["nums"]),
+                output,
+                f"{input["nums"]} - Test Case Failed")
+
 
 if __name__ == '__main__':
     unittest.main()
