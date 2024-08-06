b3e40615ae42a417d328bc2717f90f5b59500ea1
anthonyvanegas <antvanegas@icloud.com>
2024-08-06 13:20:24 -0400

    Initial Problem 347 Commit

---
+++ Problems/347.TopKFrequentElements/Solution.py
@@ -0,0 +1,8 @@
+# Submission Detail - https://leetcode.com/submissions/detail/1346014225/
+
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        pass
---
+++ Problems/347.TopKFrequentElements/TestCase.py
@@ -0,0 +1,35 @@
+import unittest
+from Solution import Solution
+
+
+class TestSuite(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.solution = Solution()
+
+    def test_example1(self):
+        input = {
+                'nums': [1, 1, 1, 2, 2, 3],
+                'k': 2,
+                }
+        output = [1, 2]
+        self.assertEqual(
+                self.solution.topKFrequent(input['nums'], input['k']),
+                output,
+                f"{input['nums'], input['k']} - Test Case Failed")
+
+    def test_example2(self):
+        input = {
+                'nums': [1],
+                'k': 1,
+                }
+        output = [1]
+        self.assertEqual(
+                self.solution.topKFrequent(input['nums'], input['k']),
+                output,
+                f"{input['nums'], input['k']} - Test Case Failed")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
